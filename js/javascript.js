e79a9681fddb7e02ce67535eb68977ef08540899
pavlo.plichko <p.plichko@joinposter.com>
2020-05-26 17:07:23 +0300

    Alpha map on DZ Mayskoe

---
+++ js/javascript.js
@@ -0,0 +1,132 @@
+var dz_data = {
+    'location': [48.48671849723088, 35.62840461730957],
+    'general_landing_zone': [
+        [48.486237, 35.630385],
+        [48.487907, 35.629177],
+        [48.488170, 35.629844],
+        [48.486449, 35.631102]
+    ],
+    'tandem_landing_zone': [
+        [48.485932, 35.629381],
+        [48.486684, 35.628790],
+        [48.486813, 35.629229],
+        [48.486053, 35.629805]
+    ]
+}
+
+var config = {
+    access_token: 'pk.eyJ1Ijoid3d3b3VuZCIsImEiOiJja2E4OGtxZWQwOXBwMnhwZzV1N3A4YTViIn0.jisawzBklAhA_seTnfVTJQ',
+
+    jumprun_len_km: 1.0,
+    jumprun_heading: 270,
+
+    first_part_len_km: 0.500,
+    second_part_len_km: 0.500,
+    third_part_len_km: 1.5,
+
+    opening_zone_width_km: 0.5,
+    ws_opening_zone_width_km: 0.30,
+    opening_zone_separation_km: 0.1,
+    wind: 0 // @TODO: use wind in calculation
+}
+
+class ZonesCalc {
+
+    _radians(degrees)
+    {
+        var pi = Math.PI;
+        return degrees * (pi/180)
+    }
+
+    _degrees(radians)
+    {
+        var pi = Math.PI;
+        return radians / (pi/180)
+    }
+
+    _getShiftedPosition(pos, heading, len)
+    {
+        // approximate radius of earth in km
+        var R = 6373.0
+
+        var shifted_lon = pos[1] + this._degrees(len * Math.sin(this._radians(heading)) / (R * Math.cos(this._radians(pos[0]))))
+        var shifted_lat = pos[0] + this._degrees(len * Math.cos(this._radians(heading)) / R)
+        return [shifted_lat, shifted_lon]
+    }
+
+    getData(config) {
+        var data = {}
+
+        var jumprun_pos = this._getShiftedPosition(
+            dz_data['location'],
+            config['jumprun_heading'], config['jumprun_len_km']
+        )
+        data['jumprun_pos'] = jumprun_pos
+
+        // Left turn pattern
+        var first_left_turn_pos = this._getShiftedPosition(jumprun_pos, config['jumprun_heading'], config['first_part_len_km'])
+        var second_left_turn_pos = this._getShiftedPosition(first_left_turn_pos, config['jumprun_heading'] - 90, config['second_part_len_km'])
+        var deploy_left_turn_pos = this._getShiftedPosition(second_left_turn_pos, config['jumprun_heading'] - 180, config['third_part_len_km'])
+        data['first_left_turn_pos'] = first_left_turn_pos
+        data['second_left_turn_pos'] = second_left_turn_pos
+        data['deploy_left_turn_pos'] = deploy_left_turn_pos
+
+        // Right turn pattern
+        var first_right_turn_pos = this._getShiftedPosition(jumprun_pos, config['jumprun_heading'], config['first_part_len_km'])
+        var second_right_turn_pos = this._getShiftedPosition(first_right_turn_pos, config['jumprun_heading'] + 90, config['second_part_len_km'])
+        var deploy_right_turn_pos = this._getShiftedPosition(second_right_turn_pos, config['jumprun_heading'] + 180, config['third_part_len_km'])
+        data['first_right_turn_pos'] = first_right_turn_pos
+        data['second_right_turn_pos'] = second_right_turn_pos
+        data['deploy_right_turn_pos'] = deploy_right_turn_pos
+
+        // General opening zone coordinates
+        data['general_opening_zone'] = [
+            this._getShiftedPosition(jumprun_pos, config['jumprun_heading'] + 90, config['opening_zone_width_km'] / 2),
+            this._getShiftedPosition(dz_data['location'], config['jumprun_heading'] + 90, config['opening_zone_width_km'] / 2),
+            this._getShiftedPosition(dz_data['location'], config['jumprun_heading'] - 90, config['opening_zone_width_km'] / 2),
+            this._getShiftedPosition(jumprun_pos, config['jumprun_heading'] - 90, config['opening_zone_width_km'] / 2)
+        ]
+
+        // WS left pattern opening zone coordinates
+        data['ws_left_opening_zone'] = [
+            this._getShiftedPosition(
+                jumprun_pos, config['jumprun_heading'] - 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km']
+            ),
+            this._getShiftedPosition(
+                jumprun_pos, config['jumprun_heading'] - 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km'] + config['ws_opening_zone_width_km']
+            ),
+            this._getShiftedPosition(
+                dz_data['location'], config['jumprun_heading'] - 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km'] + config['ws_opening_zone_width_km']
+            ),
+            this._getShiftedPosition(
+                dz_data['location'], config['jumprun_heading'] - 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km']
+            )
+        ]
+
+        // WS right pattern opening zone coordinates
+        data['ws_right_opening_zone'] = [
+            this._getShiftedPosition(
+                jumprun_pos, config['jumprun_heading'] + 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km']
+            ),
+            this._getShiftedPosition(
+                jumprun_pos, config['jumprun_heading'] + 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km'] + config['ws_opening_zone_width_km']
+            ),
+            this._getShiftedPosition(
+                dz_data['location'], config['jumprun_heading'] + 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km'] + config['ws_opening_zone_width_km']
+            ),
+            this._getShiftedPosition(
+                dz_data['location'], config['jumprun_heading'] + 90,
+                config['opening_zone_width_km'] / 2 + config['opening_zone_separation_km']
+            )
+        ]
+        return data
+    }
+
+}
